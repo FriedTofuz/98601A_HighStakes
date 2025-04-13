f1f88741eeca22e3aa6d39b4fb03db3c7dd24ec4
Allen Hsieh <allenhsieh2007@gmail.com>
2025-04-13 15:03:59 -0700

    progress goal side blue

--- src/autons.cpp
+++ src/autons.cpp
@@ -71,15 +71,18 @@ void goalSideBlue() {
 
 //
 void goalSideRed() {
-    Intake.allowed = true;
     Intake.stop();
     chassis.setPose(-25, 9.5,46);
     MogoMech.release();
+    LadyBrown.setState(1);
     Intake.setIntakeSpeed(127);
-    Intake.in(1, 0);
-    chassis.moveToPoint(0, 32, 800, {.minSpeed=110},false);
+    Intake.in(false, false);
+    chassis.moveToPoint(0, 35, 800, {.minSpeed=110},false);
+    chassis.turnToHeading(46, 500);
+    Intake.stop();
+    LadyBrown.setState(2);
     MogoArm.down();
-    chassis.moveToPoint(0, 18, 3000, {.forwards=false}, false);
+    chassis.moveToPoint(-10, 18, 3000, {.forwards=false}, false);
     MogoArm.up();
     pros::delay(400);
     chassis.moveToPoint(-28, 45, 2000, {.forwards=false, .maxSpeed=80}, false);
@@ -92,16 +95,6 @@ void goalSideRed() {
     chassis.moveToPoint(11, 36, 2000, {.forwards=false, .maxSpeed=70}, false); //second mogo
     MogoMech.clamp();
     pros::delay(300);
-    chassis.turnToHeading(-170, 700);
-    MogoArm.down();
-    Intake.setIntakeSpeed(-127);
-    chassis.moveToPoint(-2.75, 0, 1500);
-    chassis.turnToHeading(-120,800, {.maxSpeed=55});
-    Intake.setIntakeSpeed(127);
-    chassis.moveToPoint(-14, -2.5, 600, {}, false);
-    chassis.swingToHeading(-74, lemlib::DriveSide::RIGHT, 1000, {.maxSpeed=55});
-    chassis.moveToPoint(-17, 5, 1500);
-    MogoArm.up();
 }
 
 //DONE, solo wp 12pts
