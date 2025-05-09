40de441bac67c04803dd9b1dabb8ac83a4b7ef7a
rhncg <rohanchakgupta@gmail.com>
2025-05-09 11:18:24 -0500

    fixed colorsort toggle

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,11 @@ void initialize() {
             LadyBrown.liftControl();
             // Intake.intakeControl();
 
+            std::cout << Intake.discard << std::endl;
+
             pros::delay(10);
 
-            if (Intake.discardRing() && LadyBrown.currState != 1) {
+            if (Intake.discardRing() && LadyBrown.currState != 1 && Intake.discard) {
                 if (!Intake.discarding) {
                     Intake.discarding = true;
                 }
